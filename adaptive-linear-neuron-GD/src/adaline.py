6f74ffe87a55f2b7682509424a22be389a6b9fb6
Kyle Nosar <kylenosar@gmail.com>
2017-10-30 18:47:54 -0700

    fixed net_input

--- adaptive-linear-neuron-GD/src/adaline.py
+++ adaptive-linear-neuron-GD/src/adaline.py
@@ -48,22 +48,22 @@ class AdalineGD(object):
 
         """
 
-        self.w_ = np.zeros[1 + X.shape[1]]
+        self.w_ = np.zeros(1 + X.shape[1])
         self.cost_ = []
 
         for i in range(self.n_iters):
-            output = self.net_input(X)
+            #net_input = self.net_input(X)
+            output = self.net_input(X) #TODO: should not be returning nonetype(should be dot product)
             errors = (y - output)
-            self.w_[1:] += self.eta * X.T.dot(errors_)
+            self.w_[1:] += self.eta * X.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2.0
+            cost = (errors**2).sum() / 2.0
             self.cost_.append(cost)
-
         return self
 
     def net_input(self, X):
         """ Calculates the net input"""
-        np.dot(X, self.w_[1:]) + self.w_[0]
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def activation(self, X):
         """ Computes linear activation """
--- adaptive-linear-neuron-GD/src/plot.py
+++ adaptive-linear-neuron-GD/src/plot.py
@@ -10,17 +10,30 @@ from adaline import AdalineGD
 
 
 def main():
+    try:
+        df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+        'machine-learning-databases/iris/iris.data', header=None)   #load data into dataframes
+        print('Visual Check: \n %s' % df.tail())   #make sure visually that the data loaded correctly by checking the last 5 elements
+    except Exception as e:
+        print('Failed to load the data into dataframes properly!: \n %s' % e)
+        raise
+
+
+    y = df.iloc[0:100, 4].values
+    y = np.where(y =='Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
+
+    #above loads in iris dataset and should set our values to use for gradient
+    #descent
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize = (8,4))
     ada1 = AdalineGD(eta=0.01, n_iters=10).fit(X,y)
-    ax[0].plot(range(1,len(ada1.cost_) + 1),
-            np.log10(ada1.cost_), marker='o')
+    ax[0].plot(range(1,len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
     ax[0].set_xlabel('Epochs')
     ax[0].set_ylabel('log(Sum-squared-error)')
     ax[0].set_title('Adaline - Learning rate 0.01')
 
     ada2 = AdalineGD(n_iters=10, eta=0.0001).fit(X,y)
-    ax[1].plot(range(1,len(ada2.cost_) + 1),
-            ada2.cost_, marker='o')
+    ax[1].plot(range(1,len(ada2.cost_) + 1), ada2.cost_, marker='o')
     ax[1].set_xlabel('Epochs')
     ax[1].set_ylabel('Sum-squared-error')
     ax[1].set_title('Adaline - Learning rate 0.00001')
